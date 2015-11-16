c51ad09a0f4eedc54d5db3613fb92210d2fc5a3f
AlSteppenwolf <albertosuri123@gmail.com>
2015-11-15 19:06:26 -0600

    agregados scripts para manejo del aprendiz

--- Assets/Script_Control.js
+++ Assets/Script_Control.js
@@ -7,15 +7,27 @@ var textoIn : UnityEngine.UI.Text;
 var num : int;
 var codigoIngresado : String;
 
+var button: UnityEngine.UI.Button;
+
+var anim : Animator;
+var tomar_espada : boolean;
+var atacar : boolean;
+var defensa_espada : boolean;
+
 function Start () {
 	num = 1;
 	textoAp.text = "...";
 	textoMa.text = "Bienvenido joven aprendiz, si quieres sobrevivir en una batalla debes pensar rápido. Un error podría resultar en tu muerte. Empecemos con la practica de inmediato!";
 	textoIn.text = "";
+	tomar_espada = false;
+	atacar = false;
+	defensa_espada = false;
 }
 
 function Update () {
-
+	anim.SetBool("tomar_espada", tomar_espada);
+	anim.SetBool("atacar", atacar);
+	anim.SetBool("defensa_espada", defensa_espada);
 }
 
 function OnClick(){
@@ -32,36 +44,39 @@ function OnClick(){
 		case 4:
 			codigoIngresado = codigo.text;
 			if (codigoIngresado.Equals("Espada = raw_input("+'"'+"Tomar espada"+'"'+")")){
+				tomar_espada = true;
 				textoMa.text = "Bien, ahora atácame con ella. Con tu habilidad actual no podrás lastimarme.";
 				codigo.text = "";
 				textoIn.text="Escribe en el espacio de arriba\n print "+'"'+"Atacar con espada"+'"'+"";
 			}
 			else{
-				textoMa.text = "Estas mal!";
+				textoMa.text = "Estas mal! Concentrate!";
 				num--;
 			}
 			break;
 		case 5:
 			codigoIngresado = codigo.text;
 			if (codigoIngresado.Equals("print "+'"'+"Atacar con espada"+'"'+"")){
+				atacar = true;
 				textoMa.text = "Tienes fuerza, pero veamos que tan bien de defiendes.";
 				codigo.text = "";
 				textoIn.text="Escribe en el espacio de arriba print "+'"'+"Defender con espada"+'"'+"";
 			}
 			else{
-				textoMa.text = "Estas mal!";
+				textoMa.text = "Estas mal! Concentrate!";
 				num--;
 			}
 			break;
 		case 6:
 			codigoIngresado = codigo.text;
 			if (codigoIngresado.Equals("print "+'"'+"Defender con espada"+'"'+"")){
+				defensa_espada = true;
 				textoMa.text = "En el futuro necesitaras un escudo para defenderte. Un combate real será más difícil que esto, porque debes actuar según las acciones del enemigo.";
 				codigo.text = "";
 				textoIn.text ="";
 			}
 			else{
-				textoMa.text = "Estas mal!";
+				textoMa.text = "Estas mal! Concentrate!";
 				num--;
 			}
 			break;
